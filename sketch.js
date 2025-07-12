d4e7a7b20f94f59cc3b849c943003555b240e4c7
dRuivo <diogocaruivo@gmail.com>
2025-07-12 16:17:09 +0200

    feat: Fancy Clock

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-let ROWS = 8;
-let COLS = 16;
 let CLOCK_SIZE = 30; // Size of each clock in pixels
 let CLOCK_SPACING = 50; // Spacing between clocks in pixels
 
@@ -16,8 +14,10 @@ function setup() {
   myRadio = createRadio();
   myRadio.option("perlin");
   myRadio.option("sin");
+  myRadio.option("wave");
   myRadio.option("square");
-  myRadio.selected("square");
+  myRadio.option("clock");
+  myRadio.selected("clock");
 
   createP("Speed:");
   slider = createSlider(0, 255);
@@ -71,6 +71,94 @@ function draw() {
         }
       }
       break;
+    case "clock":
+      let now = new Date();
+      let hours = now.getHours();
+      let minutes = now.getMinutes();
+      let seconds = now.getSeconds();
+      let milliseconds = now.getMilliseconds();
+      let phase = ((seconds + milliseconds / 1000) / 60) ** 2;
+      let in_1_min = new Date(now.getTime() + 60 * 1000);
+      let in_1_min_hours = in_1_min.getHours();
+      let in_1_min_minutes = in_1_min.getMinutes();
+      let current_digits = [
+        Math.floor(hours / 10),
+        hours % 10,
+        Math.floor(minutes / 10),
+        minutes % 10,
+      ];
+      let in_1_min_digits = [
+        Math.floor(in_1_min_hours / 10),
+        in_1_min_hours % 10,
+        Math.floor(in_1_min_minutes / 10),
+        in_1_min_minutes % 10,
+      ];
+      console.log(current_digits, in_1_min_digits, seconds);
+      // let num = numbers6x3[0];
+      for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
+          let ang1 =
+            (2 * PI * (seconds + milliseconds / 1000)) / 60 +
+            (j * PI) / 16 +
+            (i * PI) / 8;
+          let ang2 = ang1 + PI;
+          if (i > 0 && i < 7 && j > 0 && j < 4) {
+            let c = current_digits[0];
+            let n = in_1_min_digits[0];
+            ii = i - 1;
+            jj = j - 1;
+            ang1 =
+              (clock_digits[c][ii][jj][0] * (1 - phase) +
+                phase * clock_digits[n][ii][jj][0]) *
+              PI;
+            ang2 =
+              (clock_digits[c][ii][jj][1] * (1 - phase) +
+                phase * clock_digits[n][ii][jj][1]) *
+              PI;
+          } else if (i > 0 && i < 7 && j > 3 && j < 7) {
+            let c = current_digits[1];
+            let n = in_1_min_digits[1];
+            ii = i - 1;
+            jj = j - 4;
+            ang1 =
+              (clock_digits[c][ii][jj][0] * (1 - phase) +
+                phase * clock_digits[n][ii][jj][0]) *
+              PI;
+            ang2 =
+              (clock_digits[c][ii][jj][1] * (1 - phase) +
+                phase * clock_digits[n][ii][jj][1]) *
+              PI;
+          } else if (i > 0 && i < 7 && j > 6 && j < 10) {
+            let c = current_digits[2];
+            let n = in_1_min_digits[2];
+            ii = i - 1;
+            jj = j - 7;
+            ang1 =
+              (clock_digits[c][ii][jj][0] * (1 - phase) +
+                phase * clock_digits[n][ii][jj][0]) *
+              PI;
+            ang2 =
+              (clock_digits[c][ii][jj][1] * (1 - phase) +
+                phase * clock_digits[n][ii][jj][1]) *
+              PI;
+          } else if (i > 0 && i < 7 && j > 9 && j < 13) {
+            let c = current_digits[3];
+            let n = in_1_min_digits[3];
+            ii = i - 1;
+            jj = j - 10;
+            ang1 =
+              (clock_digits[c][ii][jj][0] * (1 - phase) +
+                phase * clock_digits[n][ii][jj][0]) *
+              PI;
+            ang2 =
+              (clock_digits[c][ii][jj][1] * (1 - phase) +
+                phase * clock_digits[n][ii][jj][1]) *
+              PI;
+          }
+          clockwall.setFace(i, j, ang1, ang2);
+        }
+      }
+    //   break;
   }
   clockwall.display();
 }
