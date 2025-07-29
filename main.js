792a914928ec643a3b0b313c00263023f0bc3a92
dRuivo <diogocaruivo@gmail.com>
2025-07-29 13:05:17 +0200

    threejs clockwall

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ import * as THREE from "three";
 import Stats from "three/addons/libs/stats.module.js";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+let ROWS = 8;
+let COLS = 16;
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -11,44 +14,104 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 class AnalogClock {
-  constructor(scene, position = [0, 0, 0]) {
+  constructor(scene, position = [0, 0, 0], clock_size = 4) {
     // Draw Clock
     this.clockMesh = new THREE.Mesh(
-      new THREE.CylinderGeometry(4, 4, 1, 64),
+      new THREE.CylinderGeometry(
+        clock_size / 2,
+        clock_size / 2,
+        clock_size / 4,
+        64
+      ),
       new THREE.MeshPhongMaterial({ color: 0x0000ff })
     );
-    this.clockMesh.position.set(0, 0, -0.5);
+    this.clockMesh.position.set(
+      0 + position[0],
+      0 + position[1],
+      -clock_size / 8 + position[2]
+    );
     this.clockMesh.rotateX(Math.PI / 2);
     scene.add(this.clockMesh);
 
     // Create hands
+    const hourShape = new THREE.Shape();
+    hourShape.moveTo(-clock_size / 30, 0);
+    hourShape.lineTo(clock_size / 30, 0);
+    hourShape.lineTo(clock_size / 30, clock_size / 3);
+    hourShape.lineTo(0, clock_size / 2.5);
+    hourShape.lineTo(-clock_size / 30, clock_size / 3);
+    hourShape.lineTo(-clock_size / 30, 0);
+
+    const hourExtrudeSettings = {
+      steps: 1,
+      depth: 0.1,
+      bevelEnabled: false,
+    };
+
     this.hourHand = new THREE.Mesh(
-      new THREE.CylinderGeometry(0.1, 0.1, 2, 32),
+      new THREE.ExtrudeGeometry(hourShape, hourExtrudeSettings),
       new THREE.MeshPhongMaterial({ color: 0xff0000 })
     );
+    this.hourHand.position.set(position[0], position[1], position[2]);
+
+    const minuteShape = new THREE.Shape();
+    minuteShape.moveTo(-clock_size / 30, 0);
+    minuteShape.lineTo(clock_size / 30, 0);
+    minuteShape.lineTo(clock_size / 30, (clock_size * 2) / 5);
+    minuteShape.lineTo(0, clock_size / 2);
+    minuteShape.lineTo(-clock_size / 30, (clock_size * 2) / 5);
+    minuteShape.lineTo(-clock_size / 30, 0);
+    const minuteExtrudeSettings = {
+      steps: 1,
+      depth: 0.1,
+      bevelEnabled: false,
+    };
     this.minuteHand = new THREE.Mesh(
-      new THREE.CylinderGeometry(0.05, 0.05, 3, 32),
+      new THREE.ExtrudeGeometry(minuteShape, minuteExtrudeSettings),
       new THREE.MeshPhongMaterial({ color: 0x00ff00 })
     );
-    this.hourHand.position.set(0, 1, 0);
-    this.minuteHand.position.set(0, 1.5, 0);
+    this.minuteHand.position.set(position[0], position[1], position[2]);
 
     scene.add(this.hourHand);
     scene.add(this.minuteHand);
   }
 
   update(ang1, ang2) {
-    const current_hour_angle = this.hourHand.rotation.z;
-    const current_minute_angle = this.minuteHand.rotation.z;
+    this.hourHand.rotation.z = ang1;
+    this.minuteHand.rotation.z = ang2;
+  }
+}
 
-    let rotationMatrix = new THREE.Matrix4();
-    const axis = new THREE.Vector3(0, 0, 1); // Rotate around the Y axis
+class ClockWall {
+  constructor(
+    scene,
+    center_position = [0, 0, 0],
+    clock_size = 4,
+    clock_spacing = 0.5
+  ) {
+    this.clocks = [];
+    for (let i = 0; i < ROWS; i++) {
+      this.clocks[i] = [];
+      for (let j = 0; j < COLS; j++) {
+        this.clocks[i][j] = new AnalogClock(
+          scene,
+          [
+            center_position[0] +
+              j * (clock_size + clock_spacing) -
+              ((clock_size + clock_spacing) * (COLS - 1)) / 2,
+            center_position[1] +
+              i * (clock_size + clock_spacing) -
+              ((clock_size + clock_spacing) * (ROWS - 1)) / 2,
+            center_position[2],
+          ],
+          clock_size
+        );
+      }
+    }
+  }
 
-    rotationMatrix.makeRotationAxis(axis, ang1 - current_hour_angle);
-    this.hourHand.applyMatrix4(rotationMatrix);
-
-    rotationMatrix.makeRotationAxis(axis, ang2 - current_minute_angle);
-    this.minuteHand.applyMatrix4(rotationMatrix);
+  setFace(i, j, ang1, ang2) {
+    this.clocks[i][j].update(ang1, ang2);
   }
 }
 
@@ -76,19 +139,25 @@ scene.add(light);
 const ambientLight = new THREE.AmbientLight(0x505050); // Soft white light
 scene.add(ambientLight);
 
-const clock = new AnalogClock(scene);
+const clockwall = new ClockWall(scene, [0, 0, 0], 1.5);
 
-camera.position.z = 5;
+camera.position.z = 12;
 camera.lookAt(0, 0, 0);
 
 function animate() {
   const now = new Date();
-  const seconds = now.getSeconds();
+  const seconds = now.getSeconds() + now.getMilliseconds() / 1000;
   const minutes = now.getMinutes() + seconds / 60;
   const hours = (now.getHours() % 12) + minutes / 60; // Convert to 12-hour format
-  const hour_angle = -(hours * Math.PI) / 6; // 360 degrees / 12 hours = 30 degrees per hour
-  const minute_angle = -(minutes * Math.PI) / 30; // 360 degrees / 60 minutes = 6 degrees per minute
-  clock.update(hour_angle, minute_angle);
+  const second_angle = -(seconds * Math.PI) / 30; // 360 degrees / 60 seconds = 6 degrees per second
+  //   clock.update(hour_angle, minute_angle);
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLS; j++) {
+      let ang1 = second_angle + ((j - 7.5) * Math.PI) / 16 + (i * Math.PI) / 8;
+      let ang2 = ang1 + Math.PI;
+      clockwall.setFace(i, j, ang1, ang2);
+    }
+  }
 
   renderer.render(scene, camera);
   stats.update();
